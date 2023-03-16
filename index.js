77208e68b92c5890ebce554dd8036e592f96d8ea
Celia Manzano <celiamanzano@celiama-ltmaqvp.internal.salesforce.com>
2023-03-16 10:59:02 +0100

    fisrt commit

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const {pool} = require('./config')
+const path = require('path');
+
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+app.use(express.static(__dirname + '/public'));
+
+const getStock = (request, response) => {
+  pool.query('SELECT * FROM stock', (error, results) => {
+    if (error) {
+      throw error
+    }
+
+    const data = results.rows;
+
+var result = "Productos obtenidos: ";
+
+    data.forEach(row => {
+       result = result + "<br/>" + (`Producto: ${row.product}, ${row.units} `);
+    })
+
+    response.status(200).send(result)
+	
+    //)
+
+    //response.status(200).json(results.rows)
+  })
+}
+
+const addStock = (request, response) => {
+console.log(request.body)
+
+  const {product} = request.body
+
+  pool.query(
+    'INSERT INTO  stock(product) VALUES ($1)',
+    [product],
+    (error) => {
+      if (error) {
+        throw error
+      }
+	response.status(201).send(`Producto añadido: ${product}`)
+    },
+  )
+}
+
+const updateStock = (request, response) => {
+  const {product, units} = request.body
+
+  pool.query(
+    'UPDATE stock SET units = $1 WHERE product = $2',
+    [units, product],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Stock modificado: ${product}`)
+    }
+  )
+}
+
+
+/*
+app
+  .route('/stocks')
+  // GET endpoint
+  .get(getStock)
+  // POST endpoint
+  .post(addStock)
+*/
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/stocks', getStock)
+app.post('/stocks', addStock)
+app.post('/stocks_mod', updateStock)
+
+
+// Start server
+app.listen(process.env.PORT || 3002, () => {
+  console.log(`Server listening`)
+})
---
+++ init.sql
@@ -0,0 +1,3 @@
+create table stock (id serial primary key, product varchar(255) not null, units integer);
+insert into stock(product,units) values ('producto 1', 32);
+
